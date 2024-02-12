2f1c16d9019d1955c557a0095c7d053806feb242
kal <kennyandmeghan@gmail.com>
2024-02-12 16:54:25 -0500

    Initial Commit

---
+++ FootballSense/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FootballSense
+//
+//  Created by Kenny Albert on 2/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FootballSense/FootballSenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballSenseApp.swift
+//  FootballSense
+//
+//  Created by Kenny Albert on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct FootballSenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
